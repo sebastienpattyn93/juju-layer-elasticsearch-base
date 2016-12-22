4ab693c5962acb374936f90c7ce713101c1f9f82
jamesbeedy <jamesbeedy@gmail.com>
2016-12-22 00:09:33 +0000

    add 'blocked' status if es won't start

--- reactive/elasticsearch_base.py
+++ reactive/elasticsearch_base.py
@@ -4,7 +4,6 @@ from charms.reactive import when, when_not, set_state
 
 from charmhelpers.core.hookenv import status_set
 from charmhelpers.core.host import (
-    service_restart,
     service_running,
     service_start
 )
@@ -18,6 +17,11 @@ def install_elasticsearch(java):
     """Check for container, install elasticsearch
     """
 
+    # Workaround for container installs is to set 
+    # ES_SKIP_SET_KERNEL_PARAMETERS if in container
+    # so kernel files will not need to be modified on
+    # elasticsearch install. See
+    # https://github.com/elastic/elasticsearch/commit/32df032c5944326e351a7910a877d1992563f791
     if is_container():
         os.environ['ES_SKIP_SET_KERNEL_PARAMETERS'] = "true"
     subprocess.call(['apt', 'install', 'elasticsearch', '-y',
@@ -28,15 +32,18 @@ def install_elasticsearch(java):
 @when('elasticsearch.installed')
 @when_not('elasticsearch.running')
 def ensure_elasticsearch_running():
-    """Ensure to start elasticsearch when elasticsearch
-    is installed,but the 'elasticsearch.running' state is not set
+    """Ensure elasticsearch is started
     """
 
-    if service_running('elasticsearch'):
-        service_restart('elasticsearch')
-    else:
+    # If elasticsearch isn't running start it
+    if not service_running('elasticsearch'): 
         service_start('elasticsearch')
-    set_state('elasticsearch.running')
+    # If elasticsearch starts, were g2g, else block
+    if service_running('elasticsearch'):
+        set_state('elasticsearch.running')
+    else:
+        # If elasticsearch wont start, set blocked
+        set_state('elasticsearch.problems')
 
 
 @when('elasticsearch.installed', 'elasticsearch.running')
@@ -44,3 +51,11 @@ def set_ready_status():
     """Set ready status
     """
     status_set('active', 'Elasticsearch ready')
+
+
+@when('elasticsearch.problems')
+def blocked_due_to_problems():
+    """Set blocked status if we encounter issues
+    """
+    status_set('blocked',
+               "There are problems with elasticsearch, please debug")
